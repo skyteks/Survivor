91112e8e67f7f1eb6e99c5914a7f0c1d722e7af5
Skyteks <DanielEmig@gmx.de>
2022-10-16 19:38:07 +0200

    Add camera rig

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private Camera cam;
+
+    void Awake()
+    {
+        cam = GetComponentInChildren<Camera>();
+        if (cam != Camera.main)
+        {
+            Debug.LogError("Camera is not set to 'MainCamera'");
+        }
+    }
+
+    void LateUpdate()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        Transform player = (GameManager.instance.register.GetFirstObjectOfType(typeof(InputMovement)) as InputMovement).transform;
+        transform.position = Vector3.MoveTowards(transform.position, player.position, Time.deltaTime * 100f);
+    }
+}
